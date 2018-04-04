1fed62986bb1750602da6a1aaf323caece66e26f
unknown <arshidnippon2014@gmail.com>
2018-04-04 23:14:59 +0800

    updated by nippon

--- index-logged.php
+++ index-logged.php
@@ -110,8 +110,8 @@
 						<h2> __ </h2>
 					</div>
                                     <!--Direct user to registration page-->
-                                    <div class="w3-arrow bounce animated"><h2 style="color: white">Register Now</h2>
-						<a href="register.php"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+                                    <div class="w3-arrow bounce animated"><h2 style="color: white">Join Events</h2>
+						<a href="#events"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
 					</div>
 				</div>
 			</div>
