5671d9619402fa0c1a5ad3b765d65127aa844030
nasrikamaruddin <nasrikamaruddin@yahoo.com.my>
2018-04-05 23:07:24 +0800

    edited by nas for successfull register

--- signupsuccess.php
+++ signupsuccess.php
@@ -14,95 +14,134 @@ if(isset($_SESSION['loginUser'])) {
 }
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>1 MILLION TRADERS</title>
+<title>1 Million Traders - Universiti Putra Malaysia % PKT Logistic Group Sdn Bhd</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="utf-8">
-<meta name="keywords" content="Rock Music Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+
 <!-- bootstrap-css -->
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="src/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 <!--// bootstrap-css -->
 <!-- css -->
-<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<link rel="stylesheet" href="src/css/style.css" type="text/css" media="all" />
+<link rel="stylesheet" href="src/css/plan.css" type="text/css" media="all" />
 <!--// css -->
 <!-- gallery -->
-<link type="text/css" rel="stylesheet" href="css/cm-overlay.css" />
+<link type="text/css" rel="stylesheet" href="src/css/cm-overlay.css" />
 <!-- //gallery -->
 <!-- font-awesome icons -->
-<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href="src/css/font-awesome.css" rel="stylesheet"> 
 <!-- //font-awesome icons -->
 <!-- font -->
-<link href="//fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
-<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
+<link type="text/css" rel="stylesheet" href="src/css/fontJosefinSans.css" />
+<link type="text/css" rel="stylesheet" href="src/css/fontRobotoCondensed.css" />
 <!-- //font -->
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/bootstrap.js"></script>
+<script src="src/js/jquery-1.11.1.min.js"></script>
+<script src="src/js/bootstrap.js"></script>
+<script src="src/js/planTabs.js"></script>
+<script src="src/js/timer.js"></script>
 <script type="text/javascript">
-  jQuery(document).ready(function($) {
-    $(".scroll").click(function(event){   
-      event.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-    });
-  });
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
 </script> 
 <!-- animation -->
-<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
-<script src="js/wow.min.js"></script>
-  <script>
-     new WOW().init();
-  </script>
+<link href="src/css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="src/js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
 <!-- //animation --> 
 
 <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <![endif]-->
 </head>
-	<body>
-  
+<body>
+	<!-- banner -->
+	<div class="">
+		<div class="agileinfo-dot">
+			<div class="header">
+				<div class="container">
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="fixed" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<div class="agileits-logo">
+								<h1><a href="index.php"><img src="src/images/logo1.png" alt="" /> 1 Million Traders </a></h1>
+							</div>
+						</div>
 
-  
-    
-  <footer>
-    <div class="container">
-      <p>© 2017 Rock Music . All Rights Reserved | Design by <a href="http://w3layouts.com/"> W3layouts</a> </p>
-    </div>
-  </footer>
-  <!-- //footer -->
-  <script src="js/jarallax.js"></script>
-  <script src="js/SmoothScroll.min.js"></script>
-  <script type="text/javascript">
-    /* init Jarallax */
-    $('.jarallax').jarallax({
-      speed: 0.5,
-      imgWidth: 1366,
-      imgHeight: 768
-    })
-  </script>
-  <script src="js/responsiveslides.min.js"></script>
-  <script type="text/javascript" src="js/move-top.js"></script>
-  <script type="text/javascript" src="js/easing.js"></script>
-  <!-- here stars scrolling icon -->
-  <script type="text/javascript">
-    $(document).ready(function() {
-      /*
-        var defaults = {
-        containerID: 'toTop', // fading element id
-        containerHoverID: 'toTopHover', // fading element hover id
-        scrollSpeed: 1200,
-        easingType: 'linear' 
-        };
-      */
-                
-      $().UItoTop({ easingType: 'easeOutQuart' });
-                
-      });
-  </script>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+							<nav>
+								<ul class="nav navbar-nav">
+									<li><a href="#about" class="scroll">About</a></li>
+									<li><a href="#events" class="scroll">Events</a></li>
+									<li><a href="#news" class="scroll">News</a></li>
+									<li><a href="#news" class="scroll">Plan & Upgrade</a></li>
+									<li><a href="login.php" class="scroll">Log In</a></li>
+								</ul>
+							</nav>
+						</div>
+						<!-- /.navbar-collapse -->
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //banner -->
+		<div class="contact-form-center2">
+			<div class="w3l-heading">
+				<h3>1 Million Traders Registration</h3>
+				<div class="w3ls-border"> </div>
+			</div></br>
+			<div class="panel panel-info">
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-12"> 
+						<div class="alert alert-success">
+							<strong>Congratulations!</strong></br>
+							<p>You've signed up as one of our 1 Million Traders Members and thank you for your payment.</br>
+							Click Log In below to began a apart of 1 Million Traders.</p>
+						</div>
+						
+						<button type="button"  class="btn btn-success" style="width:10em;">Log In</button>	
+						</div>
+					</div>
+					</br>
+				</div>
+			</div>
+		</div>
+
+	<!-- //profile -->
+
+	<!-- footer -->
+	<footer style="position: fixed;left: 0;bottom: 0;width: 100%; text-align: center;">
+		<div class="container">
+			<p>© 2018 1MillionTraders . All Rights Reserved | Design by <a href="http://w3layouts.com/"> W3layouts</a> </p>
+		</div>
+	</footer>
+	<!-- //footer -->
+	<script src="src/js/SmoothScroll.min.js"></script>
+
+	<script src="src/js/responsiveslides.min.js"></script>
+	<script type="text/javascript" src="src/js/move-top.js"></script>
+	<script type="text/javascript" src="src/js/easing.js"></script>
+
+	
 <!-- //here ends scrolling icon -->
-	</body>
+</body>	
 </html>
