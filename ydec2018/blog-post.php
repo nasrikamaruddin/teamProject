004480484ec55386a3c586a4acd7fb2028e68d4c
syfqhyusoff <syfqhyusoff@gmail.com>
2018-04-07 02:53:47 +0800

    ydec 2018 landing page

---
+++ ydec2018/blog-post.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>YDEC 2018| Sponsorship</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Labs - Design Studio">
+	<meta name="keywords" content="lab, onepage, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+        <link href="img/logo2.png" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,600" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="logo">
+			<img src="img/logo.png" alt=""><!-- Logo -->
+		</div>
+		<!-- Navigation -->
+		<div class="responsive"><i class="fa fa-bars"></i></div>
+		<nav>
+			<ul class="menu-list">
+                                <li><a href="home.php">About</a></li>
+                                <li><a href="committe.php">Committee</a></li>
+                                <li><a href="rules.php">Rules & Regulation</a></li>
+				<li class="active"><a href="blog-post.php">Sponsorship</a></li>
+				<li><a href="elements.php">Log In</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Header section end -->
+
+
+	<!-- Page header -->
+	<div class="page-top-section">
+		<div class="overlay"></div>
+		<div class="container text-right">
+			<div class="page-info">
+				<h2>Sponsorship</h2>
+				<div class="page-links">
+                                    <a href="home.php">Home</a>
+					<span>Sponsorship</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Page header end-->
+
+
+	<!-- page section -->
+	<div class="page-section spad">
+		<div class="container">
+			<div class="row">
+					<!-- Single Post -->
+					<div class="single-post">
+
+						<div class="post-content">
+							<h2 class="post-title">Call for Sponsorships</h2>
+
+                                                        <p><br>Good day,  and a warm welcome to the first ever joint program of <b>Universiti Putra Malaysia</b> and <b>PKT Logistics Group Sdn Bhd – The Young Digital Entrepreneurship Challenge (YDEC) 2018.</b> <br><br>
+
+                                                            The main objective of the YDEC is to educate and nurture millions of creative and innovative university students in Malaysia to transform bricks and mortar businesses into new ventures by adopting novel digital technologies and solutions.<br><br>
+
+                                                            This is the very first time such a program is pioneered by UPM, and we aim to expand the program to other univeristies in Malaysia in its upcomming series. However, in its infancy, generous sponsorship from industry to support the  program, regardless in term of cash or in kind are most welcome. Four reasons motivate your orgazation to sponsor our YDEC 2018 program as following:<br><br>
+
+                                                            1. <b>Brand Awareness</b> – Create your brand awareness to more than 20,000 UPM students through continous YDEC 2018 promotion.<br>
+                                                            2. <b>Brand Reposition</b> – Opportunity to paint their brand in a different light with new segment of customer.<br>
+                                                            3. <b>Social Responsibility</b> – Improve your organization’s public image,  increase media coverage, and attract or retain investors.<br>
+                                                            4. <b>Web Traffic</b> – Improve your organization web traffic and potentially increase sales tapping from YDEC 2018 participants.<br><br>
+
+                                                            For cash sponsors, Please make cheques payable to <b>“BENDAHARI, UNIVERSITI PUTRA MALAYSIA“.</b> Official receipt will be issued and your organization is entitled for <b>Company Income Tax Exemption</b> as defined under section 44(6) Income Tax Act, 1967. Ref. No: LHDN.01/35/42/51/179-6, 1888.<br><br>
+
+Thank you in advance for your generousity and kind support. Your contributions make a great difference to our event and society. Please don’t hesitate to contact me directly at +03-8947 1799 (Offfice)/ +6013-3884778 (Mobile) or by sending an email message to twkoh@upm.edu.my for further information.<br><br>
+
+Yours sincerely,
+<!-- Post Author -->
+	<div class="author">
+            <div class="author-info">
+                <h2>Dr. Koh Tieng Wei<span> <br>YDEC 2018 Program Director</span></h2>
+            </div>
+						</div>
+						</div>
+
+					</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- page section end-->
+        
+        <!-- Footer section -->
+	<footer class="footer-section">
+		<h2>YDEC 2018 All rights reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></h2>
+	</footer>
+	<!-- Footer section end -->
+
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
---
+++ ydec2018/rules.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>YDEC 2018| Rules & Regulation</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Labs - Design Studio">
+	<meta name="keywords" content="lab, onepage, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+        <link href="img/logo2.png" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,700" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="logo">
+			<img src="img/logo1.png" alt=""><!-- Logo -->
+		</div>
+		<!-- Navigation -->
+		<div class="responsive"><i class="fa fa-bars"></i></div>
+		<nav>
+			<ul class="menu-list">
+                                <li><a href="home.php">About</a></li>
+                                <li><a href="committe.php">Committee</a></li>
+                                <li class="active"><a href="rules.php">Rules & Regulation</a></li>
+				<li><a href="blog-post.php">Sponsorship</a></li>
+				<li><a href="elements.php">Log In</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Header section end -->
+
+
+	<!-- Page header -->
+	<div class="page-top-section">
+		<div class="overlay"></div>
+		<div class="container text-right">
+			<div class="page-info">
+				<h2>Rules & Regulations</h2>
+				<div class="page-links">
+					<a href="home.php">Home</a>
+					<span>Rules & Regulations</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Page header end -->
+
+
+	<!-- Google map -->
+        <div class="container" align="center">
+            <img src="img/r1.jpg">
+            <img src="img/r2.jpg">
+            <img src="img/r3.jpg">            
+        </div>
+
+
+
+<!-- Footer section -->
+	<footer class="footer-section">
+		<h2>YDEC 2018 All rights reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></h2>
+	</footer>
+	<!-- Footer section end -->
+
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0YyDTa0qqOjIerob2VTIwo_XVMhrruxo"></script>
+	<script src="js/map.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
